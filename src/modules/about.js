028103e6cb0c4c4d6eb27efb4db912e7d887fca3
mohammadumar28 <mohammadumar28@gmail.com>
2020-11-14 22:55:39 +0530

    Add text content to the about page

--- src/modules/about.js
+++ src/modules/about.js
@@ -24,7 +24,22 @@ heading_cont.append(heading, aboutTxt);
 
 imgHeader.append(heading_cont);
 
-container.append(imgHeader);
+const address_container = document.createElement('div');
+address_container.classList.add('address-cont');
+
+const hours = document.createElement('h2');
+hours.textContent = `Hours & Locations`;
+
+const address = document.createElement('p');
+address.textContent = `1031 N. Fourth Street,
+                        Columbus, OH 43201`;
+
+const time = document.createElement('span');
+time.textContent = `Monday - Friday 8am - 3pm`;
+
+address_container.append(hours, address, time);
+
+container.append(imgHeader, address_container);
 
 export const aboutPage = () => {
     content.append(container);
